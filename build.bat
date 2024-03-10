ef22dd293a5116ddea1a65a77046809191546eff
Marcus Yong <marcusyqy@gmail.com>
2024-03-10 19:46:31 -0400

    Add format

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@ for %%a in (%*) do set "%%a=1"
 if not "%release%"=="1" set debug=1
 if "%debug%"=="1"   set release=0 && echo [debug mode]
 if "%release%"=="1" set debug=0 && echo [release mode]
-if "%~1"==""        echo [default mode]
+if "%~1"==""        echo [default mode] && set turtle=1
 
 set forward_flags=
 if "%debug%"=="1"   set forward_flags=debug
@@ -14,17 +14,29 @@ if "%release%"=="1"   set forward_flags=release
 
 :: build glfw
 set build_glfw=
-if "%glfw%"=="1" set build_glfw=build %forward_flags% && echo [BUILDING GLFW]
+if "%glfw%"=="1" set build_glfw= call build %forward_flags% && echo [BUILDING GLFW]
 pushd deps\glfw
 call %build_glfw%
 popd
 
 :: build imgui
 set build_imgui=
-if "%imgui%"=="1" set build_imgui=build %forward_flags% && echo [BUILDING IMGUI]
+if "%imgui%"=="1" set build_imgui= call build %forward_flags% && echo [BUILDING IMGUI]
 pushd deps\imgui
-call %build_imgui%
+%build_imgui%
 popd
+
+if "%format%" == "1" (
+echo [clang-format]
+set FILEMASK=*.c,*.cc,*.cpp,*.h,*.hh,*.hpp
+pushd src
+    for /R %%f in (%FILEMASK%) do (
+        echo ["formatting - %%f"]
+        clang-format -i %%f
+        clang-format -i %%f
+    )
+popd
+)
 
 set debug_flags= /Od /D_DEBUG /MTd
 set release_flags= /O2 /DNDEBUG /MT
@@ -42,8 +54,11 @@ set links= /link %imgui_link% %glfw_link% opengl32.lib gdi32.lib shell32.lib
 :: build turtle
 if not exist build mkdir build
 pushd build
-call cl %compile_flags% ..\src\turtle_main.cpp ..\src\backends\*.cpp /Fe:turtle.exe %links%
+set build_turtle=
+if "%turtle%"=="1" set build_turtle= call cl %compile_flags% ..\src\turtle_main.cpp ..\src\backends\*.cpp /Fe:turtle.exe %links% && echo [BUILDING TURTLE]
+%build_turtle%
 popd
+
 
 for %%a in (%*) do set "%%a=0"
 set debug=

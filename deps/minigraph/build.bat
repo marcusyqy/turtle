3bb1de9ea7c03b6ee0373d1bdcae7419b3dd7f75
Marcus Yong <marcusyqy@gmail.com>
2024-03-25 17:36:16 -0400

    new build

--- deps/minigraph/build.bat
+++ deps/minigraph/build.bat
@@ -6,6 +6,7 @@ for %%a in (%*) do set "%%a=1"
 if not "%release%"=="1" set debug=1
 if "%debug%"=="1"   set release=0 && echo [debug mode]
 if "%release%"=="1" set debug=0 && echo [release mode]
+if not "%run%"=="1" set build=1
 
 set debug_flags= /Od /D_DEBUG /MTd
 set release_flags= /O2 /DNDEBUG /MT
@@ -18,9 +19,17 @@ if "%release%"=="1" set compile_flags= %release_flags% %common_flags%
 
 if not exist build mkdir build
 
+
 pushd build
 for /f %%i in ('dir ..\examples /b /ad') do ( 
-    call cl %compile_flags% ..\examples\%%i\*.cpp /Fe:%%i.exe
+    if "%build%" == "1" call cl %compile_flags% ..\examples\%%i\*.cpp /Fe:%%i.exe
+)
+if "%run%" == "1" (
+    for /f %%i in ('dir ..\examples /b /ad') do ( 
+        echo [ RUNNING %%i ]
+        call %%i.exe
+        echo [ DONE %%i ]
+    )
 )
 popd
 

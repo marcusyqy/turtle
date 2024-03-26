7ae59433ad9d0cf9d34ffa7d570d475b21a663cd
Marcus Yong <marcusyqy@gmail.com>
2024-03-26 17:27:52 -0400

    Showing all the cards

--- src/logic.cpp
+++ src/logic.cpp
@@ -17,7 +17,7 @@ void node_editor() {
   ImGui::Text("input");
   ImNodes::EndInputAttribute();
 
-  ImNodes::BeginOutputAttribute(3, ImNodesPinShape_Triangle);
+  ImNodes::BeginOutputAttribute(3, ImNodesPinShape_TriangleFilled);
   ImGui::Indent(40);
   ImGui::Text("output");
   ImNodes::EndOutputAttribute();
@@ -45,9 +45,36 @@ void node_editor() {
   // ImNodes::EndStaticAttribute();
 
   ImNodes::EndNode();
+  ImNodes::Link(0, 3,5);
 
   // ImNodes::MiniMap();
   ImNodes::EndNodeEditor();
+
+  // resolve node editor stuff after.
+  int start_attr, end_attr;
+  if (ImNodes::IsLinkCreated(&start_attr, &end_attr))
+  {
+    std::cout << "linked " << start_attr << ", " << end_attr << std::endl;
+  }
+
+  int node_id;
+  if (ImNodes::IsNodeHovered(&node_id))
+  {
+    std::cout << "hovered " << node_id << std::endl;
+  }
+
+  // Note that since many nodes can be selected at once, we first need to query the number of
+  // selected nodes before getting them.
+  const int num_selected_nodes = ImNodes::NumSelectedNodes();
+  if (num_selected_nodes > 0)
+  {
+    static std::vector<int> selected_nodes;
+    selected_nodes.resize(num_selected_nodes);
+    ImNodes::GetSelectedNodes(selected_nodes.data());
+    for(auto x : selected_nodes) {
+      std::cout << "selected " << x << std::endl;
+    }
+  }
 
   ImGui::End();
 }
